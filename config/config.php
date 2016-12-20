3ab72aa4ae03b67d53cb29480e6df9c09fa8145e
Caz <caz841@gmail.com>
2016-12-20 23:56:46 +0000

    16

--- config/config.php
+++ config/config.php
@@ -17,10 +17,10 @@ $projectEmail = “hello”;
 $projectPhoneNumber = “123”;
 
 // Twilio API credentials
-//$accountSid = ‘MG24ce25644cc24da37b0d67b73a093688’;
+$accountSid = "MG24ce25644cc24da37b0d67b73a093688";
 $authToken = "6e91dbbee42743185c33d202da983f20"; 
 $twilioPhoneNumber = "+441158242766";
-//$siteURL="YOUR SITE URL";
+$siteURL="YOUR SITE URL";
 
 /*
 
